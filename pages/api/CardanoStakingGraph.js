99635bb886d92ebee18c2874086d9a775dd50c88
johnfloresios <cloudshell-user@ip-10-0-172-17.us-east-2.compute.internal>
2021-09-19 02:27:42 +0000

    adding div to close component

--- pages/api/CardanoStakingGraph.js
+++ pages/api/CardanoStakingGraph.js
@@ -4,7 +4,9 @@ export class CardanoStakingGraph extends Component {
 
 	  render() {
 		      return(
+			      <div>
 			            <p>Click on an emoji to view the emoji short name.</p>
+			      </div>
 			          )
 		    }
 
