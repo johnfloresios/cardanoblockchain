0b5449c82406a44fb93a00f99eee118105d71629
johnfloresios <cloudshell-user@ip-10-0-71-154.us-east-2.compute.internal>
2021-09-24 05:03:06 +0000

    remove log stmt

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import React from 'react';
 
 export default class App extends React.Component {
   render() {
-    console.log(this.props)
     return  (
     <div className="App">
       <CardanoPrice />
