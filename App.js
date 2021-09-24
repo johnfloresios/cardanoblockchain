70018d37d0a717a095abadc03b95495a55f59caa
johnfloresios <cloudshell-user@ip-10-0-111-21.us-east-2.compute.internal>
2021-09-24 02:17:25 +0000

    reorder file structure

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { CardanoPrice } from "../components/CardanoPrice";
-import { CardanoStaking } from "../components/CardanoStaking";
+import { CardanoPrice } from "./CardanoPrice";
+import { CardanoStaking } from "./CardanoStaking";
+import "./styles.css";
 import React from 'react';
 
 export default class App extends React.Component {
   render() {
+    console.log(this.props)
     return  (
     <div className="App">
       <CardanoPrice />
--- README.md
+++
@@ -1,1 +0,0 @@
-# Recharts Area Chart
