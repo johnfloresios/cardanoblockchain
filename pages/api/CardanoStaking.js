4581edc67d42eadf9652671c4d7c400fe3e46bb3
johnfloresios <cloudshell-user@ip-10-0-71-154.us-east-2.compute.internal>
2021-09-24 05:42:17 +0000

    correct css path

--- pages/api/CardanoStaking.js
+++ pages/api/CardanoStaking.js
@@ -10,7 +10,7 @@ import {
 } from "recharts";
 import React, { Component } from 'react';
 import { format, parseISO, subDays } from "date-fns";
-import '../../styles/globals.css'
+import '../../styles/globals.css';
 
 export class CardanoStaking extends React.Component {
 
