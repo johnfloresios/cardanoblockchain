0faca8ff503a67c1d823ec5c84630852c6a9e2dc
johnfloresios <cloudshell-user@ip-10-0-111-21.us-east-2.compute.internal>
2021-09-24 01:34:55 +0000

    fix import paths

--- pages/App.js
+++ pages/App.js
@@ -1,11 +1,10 @@
-import { CardanoPrice } from "./CardanoPrice";
-import { CardanoStaking } from "./CardanoStaking";
-import "./styles.css";
+import { CardanoPrice } from "../components/CardanoPrice";
+import { CardanoStaking } from "../components/CardanoStaking";
+import "../styles/styles.css";
 import React from 'react';
 
 export default class App extends React.Component {
   render() {
-    console.log(this.props)
     return  (
     <div className="App">
       <CardanoPrice />
