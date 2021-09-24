b9183882c56651613d33114f411e411c24a18ed8
johnfloresios <cloudshell-user@ip-10-0-111-21.us-east-2.compute.internal>
2021-09-24 03:39:26 +0000

    fix staking scripts

--- src/CardanoStaking.js
+++ src/CardanoStaking.js
@@ -148,13 +148,12 @@ function convertLovelace(value) {
 }
 
 function convertValue(value) {
-	  return value.toLocaleString(
-		      undefined, // leave undefined to use the visitor's browser 
-		                 // locale or a string like 'en-US' to override it.
-		  //     { minimumFractionDigits: 2 }
-		  //       )
-		  //       }
-		  //
-		  //       function convertDate(value) {
-		  //         return new Date(value * 1000);
-		  //         }
+  return value.toLocaleString(
+    undefined, 
+    { minimumFractionDigits: 2 }
+  )
+}
+
+function convertDate(value) {
+  return new Date(value * 1000);
+}
